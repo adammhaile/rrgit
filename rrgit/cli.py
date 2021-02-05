83ee2f59b760b26477ed8e80e59d94860a520b0a
Adam Haile <adammhaile@gmail.com>
2021-02-04 23:35:19 -0500

    starting on pull command

--- rrgit/cli.py
+++ rrgit/cli.py
@@ -5,6 +5,7 @@ from duetwebapi import DuetWebAPI as DWA
 from urllib.parse import urlparse
 import ipaddress
 import logging
+import datetime
 
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
@@ -38,12 +39,16 @@ class Config():
         try:
             with open(self.config_path, 'r') as f:
                 self.cfg = yaml.safe_load(f)
-                print(self.cfg)
+                if self.cfg is None:
+                    self.cfg = {}
+                    self.valid = False
+                    return
         except Exception as e:
             logging.debug(str(e))
             self.valid = False
             return
             
+        
         self.hostname = self.cfg.get('hostname', None)
         
         self.valid = True
@@ -52,7 +57,7 @@ class Config():
         self.cfg['hostname'] = self.hostname
         
         with open(self.config_path, 'w') as f:
-            yaml.safe_dump(self.cfg, default_flow_style=False, indent=2)
+            yaml.safe_dump(self.cfg, f, default_flow_style=False, indent=2)
             
 class Command():
     @staticmethod
@@ -64,6 +69,9 @@ class Command():
         self.args = args
         self.dwa = None
         self.directories = []
+        
+        if not self.cfg.valid:
+            raise rrgit_error('Error: Invalid config')
         
     def run(self):
         pass
@@ -81,7 +89,8 @@ class Command():
             dirs = self.dwa.get_model('directories')
             for d in dirs:
                 self.directories.append(dirs[d][3:-1])
-            print(self.directories)
+            self.directories = list(set(self.directories)) # remove dupes
+            logging.info(f'Connected to {self.cfg.hostname}')
         except ValueError as e:
             raise rrgit_error('')
         
@@ -98,15 +107,15 @@ class Clone(Command):
             default=os.getcwd())
         
     def __init__(self, cfg, args):
+        if cfg.hostname is not None:
+            raise rrgit_error(f'{cfg.dir} is already an rrgit directory!')
+        
+        cfg.set_hostname(args.hostname)
+        cfg.valid = True
+        
         super().__init__(cfg, args)
-        
-        if self.cfg.hostname is not None:
-            raise rrgit_error(f'{self.cfg.dir} is already an rrgit directory!')
-        
-        self.cfg.set_hostname(self.args.hostname)
-        self.cfg.valid = True
+
         self.connect()
-        logging.info(f'Connected to {self.cfg.hostname}')
         
     def run(self):
         def get_dir(path):
@@ -122,16 +131,67 @@ class Clone(Command):
                     logging.info(f'{path}/{name}')
                     data = None
                     try:
-                        data = self.dwa.get_file(name, path)
+                        data = self.dwa.get_file(name, path, True)
                     except ValueError as e:
                         logging.error(f'Error: Could not retrieve {path}/{name}')
                     
                     if data is not None:
-                        with open(os.path.join(dirpath, name), 'wb') as of:
+                        fi = self.dwa.get_fileinfo(name, path)
+                        lastmod = fi['lastModified']
+                        lm = datetime.datetime.strptime(lastmod, '%Y-%m-%dT%H:%M:%S')
+                        lm = datetime.datetime.timestamp(lm)
+                        now = datetime.datetime.timestamp(datetime.datetime.now())
+                        outpath = os.path.join(dirpath, name)
+                        with open(outpath, 'wb') as of:
                             of.write(data)
                             
+                        os.utime(outpath, (now, lm))
+                            
         for d in self.directories:
-            if d != 'www':
+            if d == 'www':
+                continue
+            logging.info(d)
+            get_dir(d)
+    
+    def finalize(self):
+        self.cfg.write()
+        
+class Pull(Command):
+    @staticmethod
+    def add_parser(sp):
+        p = sp.add_parser('pull', 
+            help='Pull changes from RRF/Duet device')
+        
+    def __init__(self, cfg, args):
+        super().__init__(cfg, args)
+        self.connect()
+        
+    def run(self):
+        def get_dir(path):
+            dirpath = os.path.join(self.cfg.dir, path)
+            os.makedirs(dirpath, exist_ok=True)
+            items = self.dwa.get_directory(path)
+            for i in items:
+                if i['type'] == 'd':
+                    logging.info(i['name'])
+                    get_dir(path + '/' + i['name'])
+                elif i['type'] == 'f':
+                    name = i['name']
+                    logging.info(f'{path}/{name}')
+                    data = None
+                    try:
+                        fi = self.dwa.get_fileinfo(name, path)
+                        fsize = fi['size']
+                        lastmod = fi['lastModified']
+                        lm = datetime.datetime.strptime(lastmod, '%Y-%m-%dT%H:%M:%S')
+                        print(lm)
+                        print(f'Size: {fsize} bytes, Last Mod: {lastmod}')
+                    except ValueError as e:
+                        raise e
+                        logging.error(f'Error: Could not retrieve info for {path}/{name}')
+                            
+        for d in self.directories:
+            if d == 'www':
                 continue
             logging.info(d)
             get_dir(d)
@@ -140,7 +200,8 @@ class Clone(Command):
         self.cfg.write()
         
 commands = {
-    'clone': Clone
+    'clone': Clone,
+    'pull': Pull
 }
 
 def parse_args():
